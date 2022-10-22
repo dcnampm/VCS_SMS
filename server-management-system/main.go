6ad794982ef1c029cff3884a78f83f6716d4a51c
dcnam.pm <phamducnamtl2001@gmail.com>
2022-10-22 22:04:27 +0700

    router

--- server-management-system/main.go
+++ server-management-system/main.go
@@ -12,11 +12,16 @@ import (
 )
 
 var (
-	server              *gin.Engine
+	server *gin.Engine
+
 	AuthController      controllers.AuthController
 	AuthRouteController routes.AuthRouteController
+
 	UserController      controllers.UserController
 	UserRouteController routes.UserRouteController
+
+	ServerController      controllers.ServerController
+	ServerRouteController routes.ServerRouteController
 )
 
 func init() {
@@ -32,6 +37,9 @@ func init() {
 
 	UserController = controllers.NewUserController(initializers.DB)
 	UserRouteController = routes.NewRouteUserController(UserController)
+
+	ServerController = controllers.NewServerController(initializers.DB)
+	ServerRouteController = routes.NewRouteServerController(ServerController)
 
 	server = gin.Default()
 }
@@ -50,11 +58,12 @@ func main() {
 
 	router := server.Group("/api")
 	router.GET("/healthchecker", func(ctx *gin.Context) {
-		message := "Welcome to Golang with Gorm and Postgres"
+		message := "VCS_SMS"
 		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
 	})
 
 	AuthRouteController.AuthRoute(router)
 	UserRouteController.UserRoute(router)
+	ServerRouteController.ServerRoute(router)
 	log.Fatal(server.Run(":" + config.ServerPort))
 }
